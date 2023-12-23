1dcd39945953d6d19512c256ba3f8e39524bf2f1
nkste <154675124+nkste@users.noreply.github.com>
2023-12-23 15:07:26 +0300

    Add files via upload

---
+++ go/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/pkg/errors"
+)
+
+type DatabaseConfig struct {
+	Host   string
+	Port   uint16
+	DBName string
+}
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("User is ?")
+	ok := scanner.Scan()
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Unable to read username\n")
+		os.Exit(1)
+	}
+	username := scanner.Text()
+	fmt.Println("Password is ?")
+	ok = scanner.Scan()
+	if !ok {
+		fmt.Fprintf(os.Stderr, "Unable to read password\n")
+		os.Exit(1)
+	}
+	password := scanner.Text()
+	fmt.Println("User is " + username)
+	fmt.Println("Password is " + password)
+
+	connConfig, err := ParseConn("connect.conf")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading connect.conf: %v\n", err)
+		os.Exit(1)
+	}
+
+	urlExample := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", username, password, connConfig.Host, connConfig.Port, connConfig.DBName)
+	conn, err := pgx.Connect(context.Background(), urlExample)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var version string
+	err = conn.QueryRow(context.Background(), "SELECT VERSION()").Scan(&version)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(version)
+}
+
+func ParseConn(filePath string) (DatabaseConfig, error) {
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	url := strings.TrimSpace(string(fileContent))
+
+	connConfig, err := pgx.ParseConfig(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error parsing URL: %v\n", err)
+		os.Exit(1)
+	}
+
+	if connConfig.Host == "" || connConfig.Port == 0 || connConfig.Database == "" {
+		err := errors.New("Warnin configuration")
+		fmt.Printf("Error configuration: %v\n", err)
+		os.Exit(1)
+	}
+
+	dbConfig := DatabaseConfig{
+		Host:   connConfig.Host,
+		Port:   connConfig.Port,
+		DBName: connConfig.Database,
+	}
+
+	return dbConfig, err
+}
